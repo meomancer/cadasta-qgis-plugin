a69da30091c34149963721b263d8f420f6cc6c9b
meomancer <meomancer@gmail.com>
2017-01-18 13:22:57 +0700

    added get questionnaire api

--- cadasta/api/questionnaire.py
+++ cadasta/api/questionnaire.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 """
-Cadasta project - **Project api.**
+Cadasta project - **Questionnaires api.**
 
 .. note:: This program is free software; you can redistribute it and/or modify
      it under the terms of the GNU General Public License as published by
@@ -16,24 +16,36 @@ from cadasta.common.setting import (
 
 __author__ = 'Irwan Fathurrahman <irwan@kartoza.com>'
 __revision__ = '$Format:%H$'
-__date__ = '20/12/16'
+__date__ = '18/01/17'
 __copyright__ = 'Copyright 2016, Cadasta'
 
 
-class Project(BaseApi):
-    """Class to fetch available project data."""
+class GetQuestionnaire(BaseApi):
+    """Class to get and put data of questionnaire."""
 
-    api_url = 'api/v1/projects/'
+    api_url = '/api/v1/organizations/%{organization_slug}s/projects/%{project_slug}s/questionnaire/'
 
-    def __init__(self, on_finished=None):
+    def __init__(self, organization_slug, project_slug, on_finished=None):
         """Constructor.
+
+        It will get Questionnaire from organization_slug and project_slug
+
+        :param organization_slug: Organization slug for Questionnaire
+        :type organization_slug: Str
+
+        :param project_slug: Project slug for Questionnaire
+        :type project_slug: Str
 
         :param on_finished: (optional) function that catch result request
         :type on_finished: Function
         """
         self.request_url = get_url_instance() + self.api_url
-        super(Project, self).__init__()
+        super(GetQuestionnaire, self).__init__(on_finished)
         self.on_finished = on_finished
+        self.request_url = self.request_url % {
+            'organization_slug': organization_slug,
+            'project_slug': project_slug
+        }
         self.connect_get()
 
     def connection_finished(self):
